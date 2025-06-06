c292f5c40eb96200746e0e985898a519046c960e
snehkr <snehkr.official@gmail.com>
2025-06-06 20:07:54 +0530

    Typo Fix

--- gfp.py
+++ gfp.py
@@ -23,8 +23,8 @@ CHECK_URLS = [
         "http://www.google.com/",
     ),
     (
-        "facebook",
-        "http://www.facebook.com/",
+        "example",
+        "http://example.com/",
     ),
 ]
 
@@ -56,8 +56,7 @@ class Source:
     def read_mech_url(self, extra_headers=None):
         headers = {
             "User-Agent": (
-                "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) "
-                "Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1"
+                "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1"
             )
         }
 
